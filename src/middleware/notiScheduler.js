67591f141af3ee1245518013a0dbac058d9b13e4
hyejungg <kimhyejung12@naver.com>
2022-03-31 19:11:05 +0900

    [add] FCM 서버 전송 실패, 재전송 시에 슬랙 알림 울리도록 추가

--- src/middleware/notiScheduler.js
+++ src/middleware/notiScheduler.js
@@ -4,12 +4,17 @@ const { SuccessMessage, ErrorMessage } = require('../utils/response');
 const Noti = require('../models/noti');
 const { Strings } = require('../utils/strings');
 const { NotiType } = require('../utils/notiType');
+const Slack = require('../lib/slack');
 
 async function sendFcmTokenToFirebase(message) {
   try {
     const response = await firebaseAdmin.messaging().sendAll(message);
     logger.info(SuccessMessage.notiFCMSend);
-    logger.info(response); //* 추후 삭제
+    Slack.sendMessage({
+      color: Slack.Colors.info,
+      title: '푸쉬 알림 성공 여부 Responses',
+      text: `\`\`\`${JSON.stringify(response)}\`\`\``,
+    });
     // failureCount 존재 시 예외처리
     if (response.failureCount > 0) {
       const failedTokens = [];
@@ -20,16 +25,34 @@ async function sendFcmTokenToFirebase(message) {
       });
       // 실패 토큰에 한하여 재전송
       const reResponse = await firebaseAdmin.messaging().sendAll(failedTokens);
-      logger.info(`${SuccessMessage.notiFCMSend}: ${reResponse}`);
-      logger.info(reResponse); //* 추후 삭제
+      logger.info(`${SuccessMessage.notiFCMSend}`);
+      Slack.sendMessage({
+        color: Slack.Colors.warning,
+        title: '푸쉬 알림 실패에 따른 재전송 성공 여부 Responses',
+        text: `\`\`\`${JSON.stringify(reResponse)}\`\`\``,
+      });
     }
     return true;
   } catch (e) {
     const firebaseError = { err: e };
     if (firebaseError.err.code == 'messaging/invalid-payload') {
       logger.error(ErrorMessage.notiFCMSendError);
+      Slack.sendMessage({
+        color: Slack.Colors.danger,
+        title: `${ErrorMessage.notiFCMSendError}`,
+      });
     } else {
       logger.error(e);
+      Slack.sendMessage({
+        color: Slack.Colors.danger,
+        title: 'Firebase FCM Server 에러',
+        fields: [
+          {
+            title: 'Error Stack:',
+            value: `\`\`\`${e}\`\`\``,
+          },
+        ],
+      });
     }
     return false;
   }
